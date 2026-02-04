10a49c54edfd3fce44fc4a638c2af96317e2f66e
取舍 <111369757+b8l8u8e8@users.noreply.github.com>
2026-02-04 11:57:37 +0800

    Refine iframe fullscreen button

--- php-site/assets/app.js
+++ php-site/assets/app.js
@@ -3956,8 +3956,11 @@ const initImageViewer = () => {
         button.setAttribute("aria-label", "全屏");
         button.setAttribute("title", "全屏");
         button.innerHTML =
-          "<svg viewBox=\"0 0 24 24\" aria-hidden=\"true\" focusable=\"false\">" +
-          "<path d=\"M7 3H3v4h2V5h2V3zm12 0h-4v2h2v2h2V3zM5 17H3v4h4v-2H5v-2zm14 2h-2v2h4v-4h-2v2z\" fill=\"currentColor\"></path>" +
+          "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" aria-hidden=\"true\" focusable=\"false\">" +
+          "<path d=\"M3 7V5a2 2 0 0 1 2-2h2\" />" +
+          "<path d=\"M17 3h2a2 2 0 0 1 2 2v2\" />" +
+          "<path d=\"M7 21H5a2 2 0 0 1-2-2v-2\" />" +
+          "<path d=\"M21 17v2a2 2 0 0 1-2 2h-2\" />" +
           "</svg>";
         const updateState = () => {
           const isFullscreen =
